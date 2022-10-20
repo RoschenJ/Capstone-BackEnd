b6c1875eec1899270d843ae41484801eb00851d4
GregCouncil <78123903+GregCouncil@users.noreply.github.com>
2022-10-20 14:10:58 -0500

    Google Vision update
    Added Google Vision API code and authentication key json

---
+++ CapstoneBackEndAPI/CapstoneBackEndAPI/gVision.cs
@@ -0,0 +1,30 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+
+namespace VisionApiDemo
+{
+    class gVision
+    {
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "./key.json");
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile("bedroom.jpg");
+            var labels = client.DetectLabels(image);
+
+            Console.WriteLine("Labels (and confidence score):");
+            Console.WriteLine(new String('=', 30));
+
+            /*foreach (var label in labels)
+            {
+                Console.WriteLine($"{label.Description} ({(int)(label.Score * 100)}%)");
+            }*/
+
+            foreach (var label in labels)
+            {
+                Console.WriteLine(label);
+            }
+            //Console.Write(labels);
+        }
+    }
+}
