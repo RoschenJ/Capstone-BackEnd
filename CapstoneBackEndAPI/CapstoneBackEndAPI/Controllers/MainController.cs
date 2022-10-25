e520bc855350c87b619e10c8c520989dfb69e8f9
Saebastion Cole <99359888+sdcole@users.noreply.github.com>
2022-10-25 12:46:31 -0500

    Added functionality to pass path and call Google Vision.
    Passing a path to the API via POST request now works. Google Vision results are printed to console.

--- CapstoneBackEndAPI/CapstoneBackEndAPI/Controllers/MainController.cs
+++ CapstoneBackEndAPI/CapstoneBackEndAPI/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 using CapstoneBackendAPI.ViewModels;
+using CapstoneBackEndAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CapstoneBackendAPI.Controllers
@@ -8,36 +9,16 @@ namespace CapstoneBackendAPI.Controllers
     [Route("api/main")]
     public class MainController : Controller
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<MainController> _logger;
-
-        public MainController(ILogger<MainController> logger)
-        {
-            _logger = logger;
-        }
         [HttpPost]
         [Route("post")]
-        public JsonResult postFunction([FromBody] NameViewModel nameModel)
+        public JsonResult getFunction([FromBody] GoogleVisionViewModel nameModel)
         {
             var model = nameModel;
-            return Json(new {success = true, result = "My name is " + model.Name });
+            model.CallGoogleVision();
+            return Json(new { success = true });
         }
+        
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
 
 
     }
---
+++ CapstoneBackEndAPI/CapstoneBackEndAPI/ViewModels/GoogleVisionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Google.Cloud.Vision.V1;
+
+using System;
+
+namespace CapstoneBackEndAPI.ViewModels
+{
+    public class GoogleVisionViewModel
+    {
+        public string path { get; set; }
+
+        public void CallGoogleVision()
+        {
+            
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "./bin/key.json");
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile(path);
+            var labels = client.DetectLabels(image);
+            foreach (var label in labels)
+            {
+                System.Diagnostics.Debug.WriteLine(label);
+            }
+            
+            
+                        
+        }
+    }
+}
