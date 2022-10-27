6980cc4247e3b313c4a265db7b1a32f7f5088d9f
GregCouncil <78123903+GregCouncil@users.noreply.github.com>
2022-10-27 14:30:01 -0500

    Update gVision.cs
    Added functionality for OCR

--- CapstoneBackEndAPI/CapstoneBackEndAPI/gVision.cs
+++ CapstoneBackEndAPI/CapstoneBackEndAPI/gVision.cs
@@ -1,5 +1,7 @@
 ﻿using Google.Cloud.Vision.V1;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace VisionApiDemo
 {
@@ -9,22 +11,34 @@ namespace VisionApiDemo
         {
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "./key.json");
             var client = ImageAnnotatorClient.Create();
-            var image = Image.FromFile("bedroom.jpg");
-            var labels = client.DetectLabels(image);
+            var image = Image.FromFile("fakereceipt.jpg");
+            //var labels = client.DetectLabels(image);
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectText(image);
+            Console.Write(textAnnotations.ElementAt(0).ToString());
+            /*foreach (EntityAnnotation text in textAnnotations)
+            {
+                Console.WriteLine($"Description: {text.Description}");
+            }*/
 
-            Console.WriteLine("Labels (and confidence score):");
-            Console.WriteLine(new String('=', 30));
+            //Console.WriteLine("Labels (and confidence score):");
+            //Console.WriteLine(new String('=', 30));
 
             /*foreach (var label in labels)
             {
                 Console.WriteLine($"{label.Description} ({(int)(label.Score * 100)}%)");
             }*/
 
-            foreach (var label in labels)
+            /*foreach (var label in labels)
             {
                 Console.WriteLine(label);
-            }
+            }*/
             //Console.Write(labels);
+            //var list = labels.ToList();
+            //Console.Write(list);
+            /*foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }*/
         }
     }
 }
